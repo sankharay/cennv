dcad0d60dcd82688b121673a773c3d42d688d697
sandeep singh <sandeepsingh@sandeeps-iMac.fios-router.home>
2016-07-09 14:12:58 -0400

    pushing logo flash
    pushing logo files

--- wp-content/themes/Trim/header.php
+++ wp-content/themes/Trim/header.php
@@ -51,7 +51,33 @@
 				<div id="top-area">
 					<?php do_action('et_header_top'); ?>
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<?php $logo = (get_option('trim_logo') <> '') ? esc_attr(get_option('trim_logo')) : get_template_directory_uri() . '/images/logo.png'; ?>
+						<div height="64" width="64" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" flashid"="" style="width:8%;float:left;&gt;
+&lt;object id=">
+  <param value="flash/logo3.swf" name="movie">
+  <param value="high" name="quality">
+  <param value="opaque" name="wmode">
+  <param value="6.0.65.0" name="swfversion">
+  <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you donâ€™t want users to see the prompt. -->
+  <param value="images/Scripts/expressInstall.swf" name="expressinstall">
+  <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+  <!--[if !IE]>-->
+  <object width="64" height="64" data="flash/logo3.swf" type="application/x-shockwave-flash">
+    <!--<![endif]-->
+    <param value="high" name="quality">
+    <param value="opaque" name="wmode">
+    <param value="6.0.65.0" name="swfversion">
+    <param value="images/Scripts/expressInstall.swf" name="expressinstall">
+    <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+    <div>
+      <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+      <p><a href="http://www.adobe.com/go/getflashplayer"><img width="112" height="33" alt="Get Adobe Flash player" src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif"></a></p>
+    </div>
+    <!--[if !IE]>-->
+  </object>
+  <!--<![endif]-->
+
+
+</div><?php $logo = (get_option('trim_logo') <> '') ? esc_attr(get_option('trim_logo')) : get_template_directory_uri() . '/images/logo.png'; ?>
 						<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" id="logo"/>
 					</a>
 				</div> <!-- end #top-area -->
